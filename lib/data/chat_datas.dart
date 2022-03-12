200c817eb70ae748bf038be06302e412bbfd65d4
Mohammed Mirsa <mirsa@kali.kali>
2022-03-12 17:23:58 +0530

    data file created and data added for chatscreen

---
+++ lib/data/chat_datas.dart
@@ -0,0 +1,65 @@
+// name, profile, message, time
+
+var chats = {
+  0: [
+    'Midlaj',
+    'images/img_avatar_1.png',
+    'Hello Brother How are you ?...',
+    '9:30 pm'
+  ],
+  1: [
+    'Anjana',
+    'images/img_avatar2.png',
+    'How is your works going on....',
+    '10:01 pm'
+  ],
+  2: [
+    'Mishal',
+    'images/img_avatar_3.png',
+    'when is return time....',
+    '06:51 pm'
+  ],
+  3: [
+    'Sam',
+    'images/img_avatar_4.png',
+    'where is my camera....',
+    '10:01 pm'
+  ],
+  4: [
+    'Aswin',
+    'images/img_avatar_5.png',
+    'is it Bug Bounty....',
+    '12:36 am'
+  ],
+  5: [
+    'Ayisha',
+    'images/img_avatar_6.png',
+    'Do you have any other...',
+    '12:36 am'
+  ],
+  6: [
+    'Mahshooq',
+    'images/img_avatar_7.png',
+    'I have some tickets for hackathon...',
+    '12:36 am'
+  ],
+  7: [
+    'Diya',
+    'images/img_avatar_8.png',
+    'Are you okay with this re...',
+    '2:16 pm'
+  ],
+  8: [
+    'Alexa',
+    'images/img_avatar_9.png',
+    'Did you complete that project...',
+    '4:31 pm'
+  ],
+  9: [
+    'Rakesh',
+    'images/img_avatar_10.png',
+    'Did you fix that bug in your....',
+    '12:36 am'
+  ],
+  
+};
